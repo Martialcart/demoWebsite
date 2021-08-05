5fe7de8d5f8c96ad861b372be2788872fa16fe82
Jan Olav Berg <janolav91@hotmail.com>
2021-08-05 13:25:24 +0200

    made folder structure flat, fomrating is correct

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
-function set_screen(url) {
-    document.getElementById('main_screen').src=url;
+function generate_menubar() {
+    add_menu_headline("meg");
+    add_menu_button("CV", "cv.html");
+
+    add_menu_headline("Prosjekt");
+    add_menu_button("Literpris Alkohol", "alko_pris.html");
+    add_menu_button("Touch", "touch.html");
+    
+}
+
+function add_menu_button(name, link) {
+    const button = document.createElement("button");
+    button.innerHTML = name;
+    button.addEventListener("click", function() {
+        if(not_same_page(link)) window.open(link, "_self");
+    });
+    document.getElementById("menu").appendChild(button);
+}
+
+function add_menu_headline(title) {
+    const headline = document.createElement("h1");
+    headline.innerHTML = title;
+    document.getElementById("menu").appendChild(headline);
+}
+
+function not_same_page(link) {
+    let url = window.location.toString();
+    let short_url = url.substring(url.length - link.length, url.length);
+
+    return short_url !== link;
 }
--- touch/touch.js
+++
@@ -1,5 +0,0 @@
-document.onkeypress = function (e) {
-    e = e || window.event;
-    // use e.keyCode
-    document.getElementById(output).innerHTML.replace(e.key);
-};
