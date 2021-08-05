b1ee5c5f12eb9d8035741f55054fd2202cfbee74
Jan Olav Berg <janolav91@hotmail.com>
2021-08-05 09:59:21 +0200

    created skeleton for jmenu functions

---
+++ alko-pris/menu_creator.js
@@ -0,0 +1,14 @@
+function generate_menubar() {
+    add_headline("meg");
+    add_button("Alko Pris", "file:alko-pris/alko_pris.html");
+    
+}
+
+function add_button(name, link) {
+
+    document.getElementById("menu").appendChild();
+}
+
+function add_headline(title) {
+    
+}
