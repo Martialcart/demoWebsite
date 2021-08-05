28f836830901d11deaa54a9c5309cf470e42e88f
Jan Olav Berg <janolav91@hotmail.com>
2021-08-05 11:59:08 +0200

    menu skips reloading when opening same page

--- alko-pris/menu_creator.js
+++ alko-pris/menu_creator.js
@@ -1,15 +1,28 @@
 function generate_menubar() {
     add_headline("meg");
-    add_button("Alko Pris", "file:alko-pris/alko_pris.html");
+    add_button("Alko Pris", "alko_pris.html");
+    add_button("google", "https://www.google.com/");
     
 }
 
 function add_button(name, link) {
     const button = document.createElement("button");
-    button.innerHTML = "test";
+    button.innerHTML = name;
+    button.addEventListener("click", function() {
+        if(not_same_page(link)) window.open(link, "_self");
+    });
     document.getElementById("menu").appendChild(button);
 }
 
 function add_headline(title) {
+    const headline = document.createElement("h1");
+    headline.innerHTML = title;
+    document.getElementById("menu").appendChild(headline);
+}
 
+function not_same_page(link) {
+    let url = window.location.toString();
+    let short_url = url.substring(url.length - link.length, url.length);
+
+    return short_url !== link;
 }
