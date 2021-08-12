4db8340b454ac5d4d3b4b1bc4b1441ecdb8ae0f5
unknown <janolav91@hotmail.com>
2021-08-12 11:54:08 +0200

    added succes and fail message in touch

--- js/touch.js
+++ js/touch.js
@@ -1,9 +1,27 @@
-let char_rec = [];
+let char_best = [];
+let char_worst = [];
+let average_reaction_time;
+let reactions = 0;
+let time_stamp = performance.now();
 
 function handle_input() {
-    get("char_result").innerHTML = get("char_input").value;
-    get("char_input").value = "";
+    const result = document.getElementById("char_result");
+    const input = document.getElementById("char_input");
+    const request = document.getElementById("char_request");
+    result.innerHTML = evaluate(request, input);
+    input.value = "";
 }
-function get(id) {
-    return document.getElementById(id);
+
+function evaluate(request, input) {
+    const succes = input.value + " is correct!";
+    const fail = input.value + " is wrong, expected " + request.value;
+    if(request.value === input.value) {
+        return succes;
+    } else {
+        return fail;
+    }
+}
+
+function update_chars() {
+    char_worst = chars.innerHTML.split();
 }
