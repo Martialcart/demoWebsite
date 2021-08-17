be7f70951d06f625cc9ed4686f015aae141113fb
Martialcart <jbe091@student.uib.no>
2021-08-17 17:23:32 +0200

    touch works, but get stuck in lop between two letters

--- js/touch.js
+++ js/touch.js
@@ -1,8 +1,8 @@
 let char_best = [];
 let char_worst = [];
 let average_reaction_time = 0;
-let reactions = 0;
-let time_stamp = performance.now();
+let reactions = 1;
+let time_stamp = performance.now(); 
 const no_char_err_msg = "Ingen treningstegn angitt";
 
 function handle_input() {
@@ -17,8 +17,14 @@ function evaluate(request, input) {
     const succes = input.value + " er riktig!";
     const fail = input.value + " er feil, forventet " + request.innerHTML;
     if(request.innerHTML === input.value.charAt(0)) {
-        char_best.push(request.innerHTML);
+        let temp = request.innerHTML;
         get_new_char_request();
+        let time = timer();
+        if(time < average_reaction_time) char_best.push(temp);
+        else char_worst.push(temp);
+        average_reaction_time = average(time);
+        console.log(average_reaction_time);
+        console.log(char_worst.length);
         return succes;
     } else {
         return fail;
@@ -38,7 +44,17 @@ function update_chars() {
 
 function get_new_char_request() {
     const requester = document.getElementById("char_request");
-    if(0 < char_worst.length) requester.innerHTML = char_worst.pop();
+    if(3 < char_worst.length) requester.innerHTML = char_worst.pop();
     else if(0 < char_best.length) requester.innerHTML = char_best.pop();
     else requester.innerHTML = no_char_err_msg;
+}
+/* returns time since last call*/ 
+function timer() {
+    let temp = time_stamp;
+    time_stamp = performance.now();
+    return time_stamp - temp;
+}
+/* ads a new value to already esitmated average*/ 
+function average(new_time) {
+    return (average_reaction_time * (reactions - 1)  + new_time) / reactions++;
 }
