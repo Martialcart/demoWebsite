829fb98306bb6b3dc8f9760a4cbb51a28ba9ee4a
Martialcart <jbe091@student.uib.no>
2021-08-18 09:25:39 +0200

    update_chars works

--- js/touch.js
+++ js/touch.js
@@ -1,8 +1,9 @@
 let char_best = [];
-let char_worst = [];
+let char_rec = [];
 let average_reaction_time = 0;
 let reactions = 1;
-let time_stamp = performance.now(); 
+let time_stamp = performance.now();
+const default_reaction_time = 40000;
 const no_char_err_msg = "Ingen treningstegn angitt";
 
 function handle_input() {
@@ -21,10 +22,10 @@ function evaluate(request, input) {
         get_new_char_request();
         let time = timer();
         if(time < average_reaction_time) char_best.push(temp);
-        else char_worst.push(temp);
+        else char_rec.push(temp);
         average_reaction_time = average(time);
         console.log(average_reaction_time);
-        console.log(char_worst.length);
+        console.log(char_rec.length);
         return succes;
     } else {
         return fail;
@@ -32,19 +33,23 @@ function evaluate(request, input) {
 }
 
 function update_chars() {
-    char_best = [];
-    char_worst = [];
+    char_rec = {};
     const chars = document.getElementById("train_char").value;
     const n_chars = chars.length;
     for (c=0; c < n_chars; c++) {
-        char_worst.push(chars.charAt(c));
+        if(!char_rec.hasOwnProperty(chars.charAt(c))) {
+            char_rec[chars.charAt(c)] = default_reaction_time;
+        }
     }
+    for (const key in char_rec){
+        console.log(key);
+      }
     get_new_char_request();
 }
 
 function get_new_char_request() {
     const requester = document.getElementById("char_request");
-    if(0 < char_worst.length) requester.innerHTML = char_worst.pop();
+    if(0 < char_rec.length) requester.innerHTML = char_rec.pop();
     else if(0 < char_best.length) requester.innerHTML = char_best.pop();
     else requester.innerHTML = no_char_err_msg;
 }
